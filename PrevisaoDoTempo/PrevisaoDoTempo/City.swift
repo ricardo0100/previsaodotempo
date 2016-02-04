711984e9513122c5f1537a80a584cb39e098bf72
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2016-02-03 22:25:08 -0200

    Reset

---
+++ PrevisaoDoTempo/PrevisaoDoTempo/City.swift
@@ -0,0 +1,25 @@
+//
+//  City.swift
+//  PrevisaoDoTempo
+//
+//  Created by Ricardo Gehrke Filho on 03/02/16.
+//  Copyright © 2016 Ricardo Gehrke Filho. All rights reserved.
+//
+
+import Foundation
+
+class City {
+    
+    var id: Int
+    var name: String
+    var state: String
+    
+    init(id: Int, name: String, state: String) {
+        
+        self.id = id
+        self.name = name
+        self.state = state
+        
+    }
+    
+}
---
+++ PrevisaoDoTempo/PrevisaoDoTempo/SearchCityViewControllerDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SearchCityViewControllerDelegate.swift
+//  PrevisaoDoTempo
+//
+//  Created by Ricardo Gehrke Filho on 03/02/16.
+//  Copyright © 2016 Ricardo Gehrke Filho. All rights reserved.
+//
+
+import Foundation
+
+protocol SearchCityViewControllerDelegate {
+    
+    func listCitiesWith(cities: [City])
+    
+}
