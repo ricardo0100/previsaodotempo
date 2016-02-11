9f20586ee8282628caa28c4f12c4306f765b7b39
Ricardo Gehrke Filho <ricardo0100@gmail.com>
2016-02-10 22:35:58 -0200

    Isolating whether caption in the API file

--- PrevisaoDoTempo/PrevisaoDoTempo/APIController.swift
+++ PrevisaoDoTempo/PrevisaoDoTempo/APIController.swift
@@ -73,12 +73,13 @@ class APIController {
                     
                     for weather in xml["cidade"]["previsao"] {
                         let date = self.dateFormater.dateFromString(weather["dia"].element!.text!)!
-                        let status = weather["tempo"].element!.text!
+                        let statusCode = weather["tempo"].element!.text!
+                        let status = self.weatherStatus[statusCode]!
                         let max = Int(weather["maxima"].element!.text!)!
                         let min = Int(weather["minima"].element!.text!)!
                         let uvi = Float(weather["iuv"].element!.text!)!
                         
-                        let weatherForDate = WeatherForDate(date: date, statusCode: status, max: max, min: min, uvi: uvi)
+                        let weatherForDate = WeatherForDate(date: date, status: status, max: max, min: min, uvi: uvi)
                         
                         cityWeather.weatherDates?.append(weatherForDate)
                     }
@@ -93,4 +94,46 @@ class APIController {
         }
     }
     
+    let weatherStatus: [String: String] = [
+        "ec": "Encoberto com Chuvas Isoladas",
+        "ci": "Chuvas Isoladas",
+        "c": "Chuva",
+        "in": "Instável",
+        "pp": "Poss. de Pancadas de Chuva",
+        "cm": "Chuva pela Manhã",
+        "cn": "Chuva a Noite",
+        "pt": "Pancadas de Chuva a Tarde",
+        "pm": "Pancadas de Chuva pela Manhã",
+        "np": "Nublado e Pancadas de Chuva",
+        "pc": "Pancadas de Chuva",
+        "pn": "Parcialmente Nublado",
+        "cv": "Chuvisco",
+        "ch": "Chuvoso",
+        "t": "Tempestade",
+        "ps": "Predomínio de Sol",
+        "e": "Encoberto",
+        "n": "Nublado",
+        "cl": "Céu Claro",
+        "nv": "Nevoeiro",
+        "g": "Geada",
+        "ne": "Neve",
+        "nd": "Não Definido",
+        "pnt": "Pancadas de Chuva a Noite",
+        "psc": "Possibilidade de Chuva",
+        "pcm": "Possibilidade de Chuva pela Manhã",
+        "pct": "Possibilidade de Chuva a Tarde",
+        "pcn": "Possibilidade de Chuva a Noite",
+        "npt": "Nublado com Pancadas a Tarde",
+        "npn": "Nublado com Pancadas a Noite",
+        "ncn": "Nublado com Poss. de Chuva a Noite",
+        "nct": "Nublado com Poss. de Chuva a Tarde",
+        "ncm": "Nubl. c/ Poss. de Chuva pela Manhã",
+        "npm": "Nublado com Pancadas pela Manhã",
+        "npp": "Nublado com Possibilidade de Chuva",
+        "vn": "Variação de Nebulosidade",
+        "ct": "Chuva a Tarde",
+        "ppn": "Poss. de Panc. de Chuva a Noite",
+        "ppt": "Poss. de Panc. de Chuva a Tarde",
+        "ppm": "Poss. de Panc. de Chuva pela Manhã"
+    ]
 }
